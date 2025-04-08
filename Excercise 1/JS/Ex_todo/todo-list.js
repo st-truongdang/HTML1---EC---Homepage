32f426a958e5d3daf7d76ff3d3c47854e2376a7c
st-truongdang <truong.dang@supremetech.vn>
2025-04-08 23:32:51 +0700

    fix review todo lít

--- Excercise 1/JS/Ex_todo/todo-list.js
+++ Excercise 1/JS/Ex_todo/todo-list.js
@@ -1,39 +1,41 @@
 // event add todo button
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("btn").onclick = addTodo;
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.btn-add-todo').onclick = addTodo;
 });
 
 //function add todo
 function addTodo(event) {
   event.preventDefault();
-  const todoInput = document.getElementById("input").value;
+  const todoInput = document.getElementById('input').value;
   console.log(todoInput);
 
-  // Create an "todo" :
-  const todo = document.createElement("li");
+  // Create an 'todo' :
+  const todo = document.createElement('li');
   todo.innerHTML = `${todoInput}`;
-  todo.classList.add("list-item");
+  todo.classList.add('list-item');
 
   // Create btn delete
-  const btnX = document.createElement("button");
-  btnX.classList.add("delBtn");
-  btnX.innerHTML = "X";
+  const btnX = document.createElement('button');
+  btnX.classList.add('btn-delete');
+  btnX.innerHTML = 'X';
 
   //create btn done
-  const btnDone = document.createElement("button");
-  btnDone.classList.add("doneBtn");
-  btnDone.innerHTML = "Toggle";
+  const btnDone = document.createElement('button');
+  btnDone.classList.add('btn-toggle');
+  btnDone.innerHTML = 'Toggle';
 
   // append btns
   todo.appendChild(btnDone);
   todo.appendChild(btnX);
 
-  document.getElementById("todo-list").appendChild(todo);
+  document.querySelector('.todo-list').appendChild(todo);
 
+  //function toggle done status
   btnDone.onclick = function toggleTodo() {
-    todo.classList.toggle("toggle");
+    todo.classList.toggle('toggle');
   };
 
+  //function remove todo
   btnX.onclick = function removeTodo() {
     todo.remove();
   };
